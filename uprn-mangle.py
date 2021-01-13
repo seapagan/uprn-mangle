7472639346274bae7b12344c79ac2ba9652e4fef
Grant Ramsay <seapagan@gmail.com>
2021-01-13 13:36:00 +0000

    First commit

---
+++ uprn-mangle.py
@@ -0,0 +1,7 @@
+# this will take the raw CSV files provide by Ordinance Survey
+# 'AddressBase Premium and tweak into more useful formats.j
+
+# it will merge all the separate 5km files into combined files with the same
+# header numbers
+# it will also create one combined file suitable for inserting into a database
+# - removing all the unneeded columns (configurable)
