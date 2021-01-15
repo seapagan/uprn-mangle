f369e25fa9f7cc8cf31bed3e0abeadbd76e91b68
Grant Ramsay <seapagan@gmail.com>
2021-01-15 17:10:04 +0000

    add some more constants
    OUTPUT_DIR - where we will put the final product
    CROSSREF_NAME - name of the UPRN<->USRN File.

--- constants.py
+++ constants.py
@@ -9,3 +9,7 @@ RAW_DIR = os.path.join(our_path, "raw-csv")
 MANGLED_DIR = os.path.join(our_path, "mangled-csv")
 HEADER_DIR = os.path.join(our_path, "header-files")
 CROSSREF_DIR = os.path.join(our_path, "cross-ref-csv")
+OUTPUT_DIR = os.path.join(our_path, "output-csv")
+
+# this name should never change
+CROSSREF_NAME = "BLPU_UPRN_Street_USRN_11.csv"
