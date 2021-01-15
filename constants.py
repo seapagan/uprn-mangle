48a8dc21f506e4860e515c3086ba100601277d97
Grant Ramsay <seapagan@gmail.com>
2021-01-15 17:16:07 +0000

    change method to get script dir again.
    This method is apparently more cross-platform and accurate.

--- constants.py
+++ constants.py
@@ -1,8 +1,9 @@
 # get script path
 import os
+from inspect import getsourcefile
 
-# our_path = os.getcwd()
-our_path = os.path.abspath(os.path.dirname(__file__))
+# our_path = os.path.abspath(os.path.dirname(__file__))
+our_path = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
 
 # set some constants
 RAW_DIR = os.path.join(our_path, "raw-csv")
@@ -13,3 +14,5 @@ OUTPUT_DIR = os.path.join(our_path, "output-csv")
 
 # this name should never change
 CROSSREF_NAME = "BLPU_UPRN_Street_USRN_11.csv"
+
+print(our_path)
