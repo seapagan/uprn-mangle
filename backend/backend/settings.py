9d8d6eab87e5300779dc9920c6b1396d8379cdfc
Grant Ramsay <seapagan@gmail.com>
2021-03-28 16:49:16 +0100

    backend - swap to PageNumberPagination style

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,8 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
 
