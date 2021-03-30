2d5012640e6f42fa0c1f8638a66709c961370066
Grant Ramsay <seapagan@gmail.com>
2021-03-30 18:16:03 +0100

    add Google maps and open StreetMap icons
    they will become links to the respective services

--- frontend/src/components/ResultHeader.js
+++ frontend/src/components/ResultHeader.js
@@ -4,14 +4,16 @@ import "../css/resultheader.css";
 
 const ResultHeader = () => {
   return (
-    <>
-      <div className="header-item">UPRN</div>
-      <div className="header-item">Address</div>
+    <div className="header-wrapper">
+      <div className="header-item uprn">UPRN</div>
+      <div className="header-item address">Address</div>
       <div className="header-item">X Coord</div>
       <div className="header-item">Y Coord</div>
       <div className="header-item">Lat</div>
       <div className="header-item">Lon</div>
-    </>
+      <div className="header-item">Google Directions</div>
+      <div className="header-item">Open StreetMap</div>
+    </div>
   );
 };
 
--- frontend/src/components/ResultItem.js
+++ frontend/src/components/ResultItem.js
@@ -1,16 +1,25 @@
 import React from "react";
+
+import { FaMap } from "react-icons/fa";
+import { SiGooglemaps } from "react-icons/si";
 import "../css/resultitem.css";
 
 const ResultItem = ({ result }) => {
   return (
-    <>
-      <div className="result uprn">{result.uprn} </div>
-      <div className="result address">{result.full_address} </div>
-      <div className="result x-coord">{result.x_coordinate} </div>
-      <div className="result y-coord">{result.y_coordinate} </div>
-      <div className="result lat">{result.latitude} </div>
-      <div className="result lon">{result.longitude} </div>
-    </>
+    <div className="result-container">
+      <div className="result uprn">{result.uprn}</div>
+      <div className="result address">{result.full_address}</div>
+      <div className="result x-coord">{result.x_coordinate}</div>
+      <div className="result y-coord">{result.y_coordinate}</div>
+      <div className="result lat">{result.latitude}</div>
+      <div className="result lon">{result.longitude}</div>
+      <div className="result link google">
+        <SiGooglemaps />
+      </div>
+      <div className="result link openstreet">
+        <FaMap />
+      </div>
+    </div>
   );
 };
 
