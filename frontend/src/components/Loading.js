6a4b5dcc9cc7b0fcc7cab63313bd395c8ade0155
Grant Ramsay <seapagan@gmail.com>
2021-03-30 12:15:34 +0100

    add a spinner to the loader

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Loader from "react-loader-spinner";
 import "../css/loading.css";
 
 const Loading = () => {
   return (
-    <div className="loading">Please enter an Address to begin your Search</div>
+    <div className="loading">
+      <Loader type="ThreeDots" color="gray" height={50} width={50} />
+      <p>Please enter an Address to begin your Search</p>
+    </div>
   );
 };
 
