f9e8af7188b62b22ad54f2a7d8b28ebb17b073f6
Grant Ramsay <seapagan@gmail.com>
2021-04-25 12:15:20 +0100

    Change Loading spinner

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -6,7 +6,7 @@ import "../css/loading.css";
 const Loading = () => {
   return (
     <div className="loading">
-      <Loader type="ThreeDots" color="gray" height={50} width={50} />
+      <Loader type="TailSpin" color="gray" height={50} width={50} />
       <p>Please enter an Address to begin your Search</p>
     </div>
   );
