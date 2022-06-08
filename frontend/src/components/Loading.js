de5062877cb98b163007f6ce21d96d9333c65dbe
Grant Ramsay <seapagan@gmail.com>
2022-06-08 10:37:20 +0100

    fix error with Loading spinner
    caused by upgrade to newest version.

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-import Loader from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
+
 import "../css/loading.css";
 
 const Loading = () => {
   return (
     <div className="loading">
-      <Loader type="TailSpin" color="gray" height={50} width={50} />
+      <TailSpin
+        ariaLabel="loading-indicator"
+        color="gray"
+        height={50}
+        width={50}
+      />
       <p>Please enter an Address to begin your Search</p>
     </div>
   );
