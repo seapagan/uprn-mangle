59c0784b981c6facbc89df188d4413ab566f532b
Grant Ramsay <seapagan@gmail.com>
2024-05-22 11:58:22 +0100

    rewrite sqlalchemy model to newer practices
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- uprn_mangle/backend/models.py
+++ uprn_mangle/backend/models.py
@@ -1,38 +1,51 @@
 """Pydantic and SQLAlchemy models for the address table."""
 
 from pydantic import BaseModel
-from sqlalchemy import BigInteger, Column, Float, Index, String
+from sqlalchemy import BigInteger, Column, Float, Index, MetaData, String
 from sqlalchemy.dialects.postgresql import TSVECTOR
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    """Base class for SQLAlchemy models."""
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
 class Address(Base):
     """SQLAlchemy model for the address table."""
 
     __tablename__ = "addressbase"
-    UPRN = Column(BigInteger, primary_key=True, index=True)
-    FULL_ADDRESS = Column(String, nullable=False)
-    SUB_BUILDING_NAME = Column(String)
-    BUILDING_NAME = Column(String)
-    BUILDING_NUMBER = Column(String)
-    THOROUGHFARE = Column(String)
-    POST_TOWN = Column(String)
-    POSTCODE = Column(String)
-    ADMINISTRATIVE_AREA = Column(String)
-    LOGICAL_STATUS = Column(String)
-    BLPU_STATE = Column(String)
-    X_COORDINATE = Column(Float)
-    Y_COORDINATE = Column(Float)
-    LATITUDE = Column(Float)
-    LONGITUDE = Column(Float)
-    COUNTRY = Column(String)
-    CLASSIFICATION_CODE = Column(String)
-    USRN = Column(String)
-    STREET_DESCRIPTION = Column(String)
-    LOCALITY = Column(String)
-    TOWN_NAME = Column(String)
+
+    UPRN: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    FULL_ADDRESS: Mapped[str] = mapped_column(String(255), nullable=False)
+    SUB_BUILDING_NAME: Mapped[str] = mapped_column(String(64))
+    BUILDING_NAME: Mapped[str] = mapped_column(String(64))
+    BUILDING_NUMBER: Mapped[str] = mapped_column(String(8))
+    THOROUGHFARE: Mapped[str] = mapped_column(String(64))
+    POST_TOWN: Mapped[str] = mapped_column(String(64))
+    POSTCODE: Mapped[str] = mapped_column(String(10))
+    ADMINISTRATIVE_AREA: Mapped[str] = mapped_column(String(64))
+    LOGICAL_STATUS: Mapped[str] = mapped_column(String(64))
+    BLPU_STATE: Mapped[str] = mapped_column(String(64))
+    X_COORDINATE: Mapped[float] = mapped_column(Float)
+    Y_COORDINATE: Mapped[float] = mapped_column(Float)
+    LATITUDE: Mapped[float] = mapped_column(Float)
+    LONGITUDE: Mapped[float] = mapped_column(Float)
+    COUNTRY: Mapped[str] = mapped_column(String(64))
+    CLASSIFICATION_CODE: Mapped[str] = mapped_column(String(64))
+    USRN: Mapped[str] = mapped_column(String(20))
+    STREET_DESCRIPTION: Mapped[str] = mapped_column(String(255))
+    LOCALITY: Mapped[str] = mapped_column(String(64))
+    TOWN_NAME: Mapped[str] = mapped_column(String(64))
     TSV = Column(TSVECTOR)
 
     __table_args__ = (
