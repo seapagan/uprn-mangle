e35e6d44196aaec83a18344ca2a52621c28ffe7f
Grant Ramsay <seapagan@gmail.com>
2024-05-29 17:48:23 +0100

    add vite react boilerplate
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ uprn_mangle/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>UPRN Mangle frontend.</h1>
+    </>
+  );
+}
+
+export default App;
