383ac2c7e569b2d837ff3faa1b21d0d47b28993c
Grant Ramsay <seapagan@gmail.com>
2024-05-31 09:12:09 +0100

    lock down cors to only local machine
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- uprn_mangle/backend/api/main.py
+++ uprn_mangle/backend/api/main.py
@@ -20,7 +20,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[Any, None]:  # noqa: ARG001
     yield
 
 
-origins = ["*"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
 
 
 app = FastAPI(lifespan=lifespan)
