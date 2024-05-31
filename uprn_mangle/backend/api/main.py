5321f0aee050d4773e351d2805ed24bc8a8a1a4f
Grant Ramsay <seapagan@gmail.com>
2024-05-31 11:39:38 +0100

    use config parameters for the api server
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- uprn_mangle/backend/api/main.py
+++ uprn_mangle/backend/api/main.py
@@ -3,13 +3,17 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import Any
+from urllib.parse import urlparse
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
 from uprn_mangle.backend.api.routes import router
+from uprn_mangle.backend.config import get_settings
 from uprn_mangle.backend.database import init_models
+
+settings = get_settings()
 
 
 @asynccontextmanager
@@ -19,10 +23,19 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[Any, None]:  # noqa: ARG001
     yield
 
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(router, prefix="/api/v2")
+app = FastAPI(
+    lifespan=lifespan,
+    swagger_ui_parameters={"defaultModelsExpandDepth": 0},
+)
+app.include_router(router, prefix=settings.api_prefix)
 
 add_pagination(app)
 
 if __name__ == "__main__":
-    uvicorn.run("uprn_mangle.backend.api.main:app", reload=True)
+    host = urlparse(settings.api_base_url).hostname
+    uvicorn.run(
+        "uprn_mangle.backend.api.main:app",
+        reload=True,
+        port=settings.api_port,
+        host=host,
+    )
