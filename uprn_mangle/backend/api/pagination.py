b58ca8cfe9baf7c7a5a3bc5734adbe91d1ca8794
Grant Ramsay <seapagan@gmail.com>
2026-02-04 19:20:03 +0000

    fix crash  d/t new fastapi-pagination version
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- uprn_mangle/backend/api/pagination.py
+++ uprn_mangle/backend/api/pagination.py
@@ -2,37 +2,20 @@
 
 from typing import TypeVar
 
-from fastapi import Request
 from fastapi_pagination.customization import (
     CustomizedPage,
     UseFieldsAliases,
     UseParamsFields,
 )
-from fastapi_pagination.links import Page
-from fastapi_pagination.links.bases import Links
+from fastapi_pagination.default import Page
+from fastapi_pagination.links import UseLinks
 
 T = TypeVar("T")
 
 
 Pagination = CustomizedPage[
     Page[T],
+    UseLinks(only_path=False),
     UseParamsFields(size=20),
     UseFieldsAliases(items="addresses"),
 ]
-
-
-def fix_links(request: Request, links: Links) -> Links:
-    """Fix the links to add the correct base URL."""
-    base_url = str(request.base_url).rstrip("/")
-    for attr in [
-        "next",
-        "prev",
-        "first",
-        "last",
-        "self",
-    ]:
-        link = getattr(links, attr)
-        if link:
-            setattr(links, attr, f"{base_url}{link}")
-
-    return links
